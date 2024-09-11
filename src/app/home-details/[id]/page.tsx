ad94fb9119f0de1c33e6c386199e041b8ebbb933
Bolaji06 <joshspencer0603@gmail.com>
2024-09-11 21:31:35 +0100

    create a separate component for the chat and hide and open the chat

--- src/app/home-details/[id]/page.tsx
+++ src/app/home-details/[id]/page.tsx
@@ -26,7 +26,7 @@ import { convertToCurrency } from "@/lib/utils";
 import { getUserSession } from "@/lib/getSession";
 import LoginSignUp from "@/components/LoginSignUp";
 import NavBar from "@/components/ui/NavBar";
-import { Modal, SavedButton, ShareButton } from "@/components/Utilities";
+import { ChatButton, Modal, SavedButton, ShareButton } from "@/components/Utilities";
 import { cookies } from "next/headers";
 import QRCodeGenerator from "@/components/ui/qrcode";
 import { Metadata } from "next";
@@ -36,6 +36,7 @@ import dynamic from "next/dynamic";
 import PhotoGrid from "@/components/PhotoGrid";
 import { Amenities, AmenitiesHeader } from "@/components/ui/amenities";
 import { Suspense } from "react";
+import ChatComponent from "@/components/ChatComponent";
 
 export async function generateMetadata({
   params,
@@ -86,7 +87,7 @@ export default async function HomeDetailsPage({
                   </Link>
                 </Button>
               </header>
-              <div className="overflow-hidden max-h-screen relative z-30">
+              <div className="overflow-hidden max-h-screen relative z-20">
                 <PhotoGrid data={post?.post} />
               </div>
             </section>
@@ -351,130 +352,12 @@ export default async function HomeDetailsPage({
                   </Button>
                 </div>
 
-                <div className="w-full">
-                  <Button className="w-full rounded-2xl text-left inline-flex gap-3 bg-white lg:bg-transparent text-brand-primary border border-blue-700 hover:bg-blue-100">
-                    <MessageCircle size={16} />
-                    Chat {post.post.user && post.post.user?.username}
-                  </Button>
-                </div>
+                <ChatButton post={post.post}/>
               </div>
             </div>
           </div>
         </section>
-
-        <div
-          className="chat-layout fixed z-50 bottom-8 right-7 min-w-96 h-[480px] max-h-[440px] bg-slate-100
-        rounded-2xl overflow-hidden shadow-2xl"
-        >
-          <header className="px-3 py-2 border-b border-slate-300 flex gap-3 items-center">
-            <div className="">
-              <Image
-                src={post.post.user.avatar}
-                alt="user profile image"
-                width={500}
-                height={500}
-                className="h-9 w-9 aspect-square rounded-full"
-              />
-            </div>
-
-            <div>
-              <p className="text-sm font-semibold">{post.post.user.username}</p>
-              <p className="online-status capitalize text-xs text-muted-foreground">
-                Offline
-              </p>
-            </div>
-          </header>
-
-          <div className="mt-3 pb-10 px-3 py-1 relative max-h-80 overflow-y-auto">
-            <div className="py-2 px-3 mb-3 text-sm rounded-2xl bg-sky-300/20 max-w-56">
-              <div>
-                <p>
-                  Hi Josh i want enquire about the house you posted on Nest am
-                  hoping to get your phone number
-                </p>
-              </div>
-
-              <div className="flex justify-between pt-1">
-                <div>
-                  <p className="text-xs italic text-muted-foreground">
-                    2 min ago
-                  </p>
-                </div>
-
-                <div></div>
-              </div>
-            </div>
-
-            <div className="py-2 px-3 mb-3 text-sm rounded-2xl ml-[9rem] bg-green-300/20 max-w-56">
-              <div>
-                <p>Hi Josh i want enquire about the house you posted on Nest</p>
-              </div>
-
-              <div className="flex justify-between pt-1">
-                <div>
-                  <p className="text-xs italic text-muted-foreground">
-                    2 min ago
-                  </p>
-                </div>
-
-                <div></div>
-              </div>
-            </div>
-
-
-            <div className="py-2 px-3 mb-3 text-sm rounded-2xl bg-sky-300/20 max-w-56">
-              <div>
-                <p>
-                  Hi Josh i want enquire about the house you posted on Nest am
-                  hoping to get your phone number
-                </p>
-              </div>
-
-              <div className="flex justify-between pt-1">
-                <div>
-                  <p className="text-xs italic text-muted-foreground">
-                    2 min ago
-                  </p>
-                </div>
-
-                <div></div>
-              </div>
-            </div>
-
-            
-            <div className="py-2 px-3 mb-3 text-sm rounded-2xl ml-[9rem] bg-green-300/20 max-w-56">
-              <div>
-                <p>Hi Josh i want enquire about the house you posted on Nest</p>
-              </div>
-
-              <div className="flex justify-between pt-1">
-                <div>
-                  <p className="text-xs italic text-muted-foreground">
-                    2 min ago
-                  </p>
-                </div>
-
-                <div></div>
-              </div>
-            </div>
-          </div>
-
-          <div className="px-3 max-h-52 border border-slate-300 focus-within:outline focus-within:outline-blue-500  rounded-3xl mx-3 mb-3">
-            <div className="flex gap-2 justify-between items-center w-full h-full">
-              <textarea className="w-full text-sm hide-scroll py-1 resize-none max-h-32 focus:border-none focus:outline-none  bg-transparent">
-
-              </textarea>
-              
-              <Button className="bg-transparent p-2 aspect-square text-center hover:bg-blue-300/20 text-brand-primary rounded-full">
-                <Send size={18}/>
-              </Button>
-                
-             
-
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
     </>
   );
 }
